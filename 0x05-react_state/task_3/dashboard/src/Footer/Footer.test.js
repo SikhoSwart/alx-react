6fba466a5b44dd6b690f41725ac88f9e215eaad8
Sikho <129541577+SikhoSwart@users.noreply.github.com>
2024-03-23 18:34:40 +0200

    test.js

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,16 +1,75 @@
-import { shallow } from "enzyme";
 import React from "react";
+import { shallow } from "enzyme";
 import Footer from "./Footer";
+import { StyleSheetTestUtils } from "aphrodite";
 
-describe("<Footer />", () => {
-  it("Footer renders without crashing", () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.exists()).toEqual(true);
+describe("Testing <Footer /> component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<Footer />);
   });
-  it("Verify that the components at the very least render the text “Copyright”", () => {
-    const wrapper = shallow(<Footer />);
-    wrapper.update();
-    expect(wrapper.find("div.footer p")).toHaveLength(1);
-    expect(wrapper.find("div.footer p").text()).toContain("Copyright");
+
+  it("Footer Component renders without crashing", () => {
+    expect(wrapper.exists());
+  });
+
+  it("Footer compoenent render at the very least the text “Copyright”", () => {
+    expect(wrapper.find("Copyright").at(0)).toBeDefined();
+  });
+});
+
+describe("Testing Footer Component context and state", () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Tests that there is no link rendered when user is logged out within context", () => {
+    const context = {
+      user: {
+        email: "",
+        password: "",
+        isLoggedIn: false,
+      },
+    };
+
+    const wrapper = mount(
+      <AppContext.Provider value={context}>
+        <Footer />
+      </AppContext.Provider>
+    );
+
+    expect(wrapper.find("a").length).toBe(0);
+    expect(wrapper.find("a").exists()).toBe(false);
+    expect(wrapper.text()).not.toContain("Contact us");
+
+    wrapper.unmount();
+  });
+
+  it("Tests that there is a link rendered when user is logged in within context", () => {
+    const context = {
+      user: {
+        email: "",
+        password: "",
+        isLoggedIn: true,
+      },
+    };
+
+    const wrapper = mount(
+      <AppContext.Provider value={context}>
+        <Footer />
+      </AppContext.Provider>
+    );
+
+    expect(wrapper.find("a").length).toBe(1);
+    expect(wrapper.find("a").exists()).toBe(true);
+    expect(wrapper.text()).toContain("Contact us");
+
+    wrapper.unmount();
   });
 });
