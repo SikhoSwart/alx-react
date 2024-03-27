9abcbe0185bcf71acb385464015e833e7e1e6db8
Sikho <129541577+SikhoSwart@users.noreply.github.com>
2024-03-27 22:41:29 +0200

    Task 4

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,5 +1,14 @@
-import { List, Map } from 'immutable';
+import { Map } from 'immutable';
 
-export const concatElements = (map, map2) => List(map).concat(List(map2));
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
 
-export const mergeElements = (map, map2) => Map(map).merge(Map(map2));
+export const map2 = map.withMutations((mapItem) => {
+  mapItem.set(2, 'Benjamin').set(4, 'Oliver');
+});
