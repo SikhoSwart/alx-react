aeba9e39b034a8640e046617a92e7588638525a8
Sikho <129541577+SikhoSwart@users.noreply.github.com>
2024-04-04 00:34:02 +0200

    uiActionCreators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,5 @@
 import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import fetch from "node-fetch";
 
 export function login(email, password) {
   return {
@@ -7,12 +8,43 @@ export function login(email, password) {
   };
 }
 
+export const boundLogin = (email, password) => dispatch(login(email, password));
+
 export const logout = () => ({ type: LOGOUT });
+
+export const boundLogout = () => dispatch(logout());
 
 export const displayNotificationDrawer = () => ({
   type: DISPLAY_NOTIFICATION_DRAWER,
 });
 
+export const boundDisplayNotificationDrawer = () => dispatch(displayNotificationDrawer());
+
 export const hideNotificationDrawer = () => ({
   type: HIDE_NOTIFICATION_DRAWER,
 });
+
+export const boundHideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+
+export function loginSuccess() {
+  return {
+    type: LOGIN_SUCCESS,
+  };
+}
+
+export function loginFailure() {
+  return {
+    type: LOGIN_FAILURE,
+  };
+}
+
+export function loginRequest(email, password) {
+  return (dispatch) => {
+    boundLogin(email, password);
+
+    return fetch("http://localhost:8564/login-success.json")
+      .then((res) => res.json())
+      .then((json) => dispatch(loginSuccess()))
+      .catch((error) => dispatch(loginFailure()));
+  };
+}
